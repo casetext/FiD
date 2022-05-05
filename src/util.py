085d89d8bc9ee2ec60945c708f6b4247ff7b22cd
martingajek <martin@casetext.com>
2022-05-05 07:55:58 -0700

    reload model without optimizer states

--- src/util.py
+++ src/util.py
@@ -76,20 +76,25 @@ def load(model_class, dir_path, opt, reset_params=False):
     model = model_class.from_pretrained(epoch_path)
     model = model.to(opt.device)
     logger.info("loading checkpoint %s" %optimizer_path)
-    checkpoint = torch.load(optimizer_path, map_location=opt.device)
-    opt_checkpoint = checkpoint["opt"]
-    step = checkpoint["step"]
-    if "best_eval_metric" in checkpoint:
-        best_eval_metric = checkpoint["best_eval_metric"]
+    if os.path.exists(optimizer_path):
+        checkpoint = torch.load(optimizer_path, map_location=opt.device)
+        opt_checkpoint = checkpoint["opt"]
+        step = checkpoint["step"]
+        if "best_eval_metric" in checkpoint:
+            best_eval_metric = checkpoint["best_eval_metric"]
+        else:
+            best_eval_metric = checkpoint["best_dev_em"]
+        if not reset_params:
+            optimizer, scheduler = set_optim(opt_checkpoint, model)
+            scheduler.load_state_dict(checkpoint["scheduler"])
+            optimizer.load_state_dict(checkpoint["optimizer"])
+        else:
+            optimizer, scheduler = set_optim(opt, model)
     else:
-        best_eval_metric = checkpoint["best_dev_em"]
-    if not reset_params:
-        optimizer, scheduler = set_optim(opt_checkpoint, model)
-        scheduler.load_state_dict(checkpoint["scheduler"])
-        optimizer.load_state_dict(checkpoint["optimizer"])
-    else:
+        step, best_eval_metric = 0, 0.0
+        opt_checkpoint= None
         optimizer, scheduler = set_optim(opt, model)
-
+    
     return model, optimizer, scheduler, opt_checkpoint, step, best_eval_metric
 
 class WarmupLinearScheduler(torch.optim.lr_scheduler.LambdaLR):
